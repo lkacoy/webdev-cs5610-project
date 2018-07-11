60af54718440bfc9f107f6968e8c029c147f66df
lkacoy <lkacoyannakis@gmail.com>
2018-07-11 18:17:24 -0400

    add comments endpointns

--- models/comment/comment.model.server.js
+++ models/comment/comment.model.server.js
@@ -1,11 +1,32 @@
 var mongoose = require('mongoose');
-var commentSchema = require('./comment.schema.server.schema.server');
-var commentModel = mongoose.model('UserModel', commentSchema);
+var commentSchema = require('./comment.schema.server');
+var commentModel = mongoose.model('CommentModel', commentSchema);
 
 function createComment(comment) {
     return commentModel.create(comment);
 }
 
+function findAllComments() {
+    return commentModel.find({});
+}
+
+function findCommentById(commentId) {
+    return commentModel.findById(commentId);
+}
+
+function findAllCommentsByPostId(postId) {
+    return commentModel.find({postId: postId});
+}
+
+function deleteComment(comment) {
+    return commentModel.delete(comment);
+}
+
+
 module.exports = {
-    createComment: createComment
+    createComment: createComment,
+    findAllComments: findAllComments,
+    findCommentById: findCommentById,
+    findAllCommentsByPostId, findAllCommentsByPostId,
+    deleteComment: deleteComment
 };
---
+++ services/comment.service.server.js
@@ -0,0 +1,48 @@
+module.exports = function (app) {
+    app.post('/api/comment', createComment);
+    app.get('/api/comment', findAllComments);
+    app.get('/api/comment/:commentId', findCommentById);
+    app.get('/api/comment/:postId', findAllCommentsByPostId);
+    app.delete('/api/comment/:commentId', deleteComment);
+
+    var commentModel = require('../models/comment/comment.model.server');
+
+    function createComment(req, res) {
+        var comment = req.body;
+        comment = {
+            username: comment.username,
+        };
+        commentModel.createComment(comment)
+            .then(function(comment) {
+                res.json(comment);
+            });
+    }
+
+    function findAllComments(req, res) {
+        commentModel.findAllComments()
+            .then(function (comments) {
+                res.send(comments);
+            });
+    }
+
+    function findCommentById(req, res) {
+        var id = req.params['commentId'];
+        commentModel.findCommentById(id)
+            .then(function (comment) {
+                res.json(comment);
+            })
+    }
+
+    function findAllCommentsByPostId(req, res) {
+        var id = req.params['postId'];
+        commentModel.findAllCommentsByPostId(id)
+            .then(function (comments) {
+                res.json(comments);
+            })
+    }
+
+    function deleteComment(req, res) {
+        var id = req.params['commentId'];
+        commentModel.deleteComment(id);
+    }
+};
