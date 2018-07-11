43cf2f5cc1ca0ad0bbf8d957892726e47bb487e5
lkacoy <lkacoyannakis@gmail.com>
2018-07-10 21:40:40 -0400

    change variable so it makes more sense

--- models/user/user.model.server.js
+++ models/user/user.model.server.js
@@ -2,8 +2,8 @@ var mongoose = require('mongoose');
 var userSchema = require('./user.schema.server');
 var userModel = mongoose.model('UserModel', userSchema);
 
-function createUser(movie) {
-    return userModel.create(movie);
+function createUser(user) {
+    return userModel.create(user);
 }
 
 module.exports = {
