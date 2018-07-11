e916185bf8b89393b122e0778a18c41729c80d8b
lkacoy <lkacoyannakis@gmail.com>
2018-07-10 21:19:52 -0400

    add a hello world

--- server.js
+++ server.js
@@ -3,13 +3,13 @@ var app = express();
 var bodyParser = require('body-parser');
 
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/poc-omdbapi');
+mongoose.connect('mongodb://localhost/poc-blogProject');
 
 
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
-/*app.use(function(req, res, next) {
+app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin",
         "http://localhost:4200");
     res.header("Access-Control-Allow-Headers",
@@ -20,10 +20,21 @@ app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x
     next();
 });
 
+var session = require('express-session')
+app.use(session({
+    resave: false,
+    saveUninitialized: true,
+    secret: 'any string'
+}));
+
+
+app.get('/', function (req, res) {
+    res.send('Hello World')
+})
 
 
 
-require('./services/movie.service.server')(app);
+
+/*require('./services/movie.service.server')(app);
 require('./services/form.service.server')(app);*/
-
 app.listen(3000);
