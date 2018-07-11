2290d1f8dff55d354a72c08808ecc04d6ef7efe4
lkacoy <lkacoyannakis@gmail.com>
2018-07-11 18:34:57 -0400

    add some post endpoinnts

--- models/post/post.model.server.js
+++ models/post/post.model.server.js
@@ -1,11 +1,31 @@
 var mongoose = require('mongoose');
-var postSchema = require('./post.schema.server.schema.server');
+var postSchema = require('./post.schema.server');
 var postModel = mongoose.model('PostModel', postSchema);
 
 function createPost(post) {
     return postModel.create(post);
 }
 
+function findAllPosts() {
+    return postModel.find({});
+}
+
+function findPostById(postId) {
+    return postModel.findById(postId);
+}
+
+function findAllPostsByUsername(username) {
+    return postModel.find({username: username});
+}
+
+function deletePost(postId) {
+    return postModel.deleteOne(postId);
+}
+
 module.exports = {
-    createPost: createPost
+    createPost: createPost,
+    findAllPosts: findAllPosts,
+    findPostById: findPostById,
+    findAllPostsByUsername: findAllPostsByUsername,
+    deletePost: deletePost
 };
---
+++ services/post.service.server.js
@@ -0,0 +1,48 @@
+module.exports = function (app) {
+    app.post('/api/post', createPost);
+    app.get('/api/post', findAllPosts);
+    app.get('/api/post/:postId', findPostById);
+    app.get('/api/post/:username', findAllPostsByUsername);
+    app.delete('/api/post/:postId', deletePost);
+
+    var postModel = require('../models/post/post.model.server');
+
+    function createPost(req, res) {
+        var post = req.body;
+        post = {
+            username: post.username,
+        };
+        postModel.createPost(post)
+            .then(function(post) {
+                res.json(post);
+            });
+    }
+
+    function findAllPosts(req, res) {
+        postModel.findAllPosts()
+            .then(function (posts) {
+                res.send(posts);
+            });
+    }
+
+    function findPostById(req, res) {
+        var id = req.params['postId'];
+        postModel.findPostById(id)
+            .then(function (post) {
+                res.json(post);
+            })
+    }
+
+    function findAllPostsByUsername(req, res) {
+        var id = req.params['username'];
+        postModel.findAllPostsByUsername(id)
+            .then(function (posts) {
+                res.json(posts);
+            })
+    }
+
+    function deletePost(req, res) {
+        var id = req.params['postId'];
+        postModel.deletePost(id);
+    }
+};
