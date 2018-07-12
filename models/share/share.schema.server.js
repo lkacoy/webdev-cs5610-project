ebe51a53f645ffae456902b1c6697279ed7c6a68
lkacoy <lkacoyannakis@gmail.com>
2018-07-11 22:58:42 -0400

    actually put the correct type of content into share schema

--- models/share/share.schema.server.js
+++ models/share/share.schema.server.js
@@ -1,57 +1,7 @@
-module.exports = function (app) {
-    app.post('/api/share', createShare);
-    app.get('/api/share', findAllShares);
-    app.get('/api/share/:shareId', findShareById);
-    app.get('/api/share/user/:username', findAllSharesByUsername);
-    app.get('/api/share/:postId', findAllSharesForPost);
-    app.delete('/api/share/:shareId', deleteShare);
-
-    var shareModel = require('../models/share/share.model.server');
-
-    function createShare(req, res) {
-        var share = req.body;
-        share = {
-            username: share.username,
-        };
-        shareModel.createShare(share)
-            .then(function(share) {
-                res.json(share);
-            });
-    }
-
-    function findAllShares(req, res) {
-        shareModel.findAllShares()
-            .then(function (shares) {
-                res.send(shares);
-            });
-    }
-
-    function findShareById(req, res) {
-        var id = req.params['shareId'];
-        shareModel.findShareById(id)
-            .then(function (share) {
-                res.json(share);
-            })
-    }
-
-    function findAllSharesByUsername(req, res) {
-        var id = req.params['username'];
-        shareModel.findAllSharesByUsername(id)
-            .then(function (shares) {
-                res.json(shares);
-            })
-    }
-
-    function findAllSharesForPost(req, res) {
-        var id = req.params['postId'];
-        shareModel.findAllSharesForPost(id)
-            .then(function (shares) {
-                res.json(shares);
-            });
-    }
-
-    function deleteShare(req, res) {
-        var id = req.params['shareId'];
-        shareModel.deleteShare(id);
-    }
-};
+var monogoose = require('mongoose');
+var shareSchema = monogoose.Schema({
+    id: Number,
+    username: String,
+    dateShared: { type: Date, default: Date.now }
+}, {collection: 'share'});
+module.exports = shareSchema;
