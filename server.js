7ead4895f0592bca84b9845a74f12ed915ba500b
lkacoy <lkacoyannakis@gmail.com>
2018-07-30 19:57:23 -0400

    add sessions for login

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 var express = require('express');
-var app = express();
 var bodyParser = require('body-parser');
-
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://admin:admin1@ds159631.mlab.com:59631/heroku_kw3mvrqs');
 
 
-app.use(bodyParser.json()); // for parsing application/json
-app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin",
@@ -21,9 +21,38 @@ app.use(function(req, res, next) {
 });
 
 
+
+
+var session = require('express-session');
+app.use(session({
+    resave: false,
+    saveUninitialized: true,
+    secret: 'any string',
+    cookie:{maxAge:1800000}
+}));
+
+
 app.get('/', function (req, res) {
     res.send('Hello World')
 });
+
+app.get('/api/session/set/:name/:value',
+    setSession);
+app.get('/api/session/get/:name',
+    getSession);
+
+function setSession(req, res) {
+    var name = req.params['name'];
+    var value = req.params['value'];
+    req.session[name] = value;
+    res.send(req.session);
+}
+
+function getSession(req, res) {
+    var name = req.params['name'];
+    var value = req.session[name];
+    res.send(value);
+}
 
 
 require('./services/adminUser.service.server')(app);
