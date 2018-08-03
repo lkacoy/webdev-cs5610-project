c6fa171f9050ac9277accca6ac992286e2071d99
lkacoy <lkacoyannakis@gmail.com>
2018-08-02 22:18:36 -0400

    update share model

--- models/share/share.schema.server.js
+++ models/share/share.schema.server.js
@@ -1,7 +1,9 @@
-var monogoose = require('mongoose');
-var shareSchema = monogoose.Schema({
+var mongoose = require('mongoose');
+var shareSchema = mongoose.Schema({
     id: Number,
     username: String,
-    dateShared: { type: Date, default: Date.now }
+    dateShared: { type: Date, default: Date.now },
+    postId: {type: mongoose.Schema.ObjectId},
+    author: String
 }, {collection: 'share'});
 module.exports = shareSchema;
